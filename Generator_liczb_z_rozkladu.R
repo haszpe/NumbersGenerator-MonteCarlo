f8a19d29e36245930e6fe226fa5c6a390e9d13a0
HP <hanna.peciak@gmail.com>
2024-01-08 10:25:57 +0100

    Update ex. scheme

--- Generator_liczb_z_rozkladu.R
+++ Generator_liczb_z_rozkladu.R
@@ -5,8 +5,20 @@ Generator liczb losowych z zadanego rozkładu
 Autorzy projektu:
 Hanna Peciak          113752 
 Paweł Zakrzewski      ......'
+
 #----Wczytanie funkcji----------------------------------------------------------
 
 source("Functions.R")
 
-#----Generator liczb------------------------------------------------------------
+
+#----Generatory liczb losowych z rozkładu normalnego----------------------------
+
+#----Metoda dystrybuanty odwrotnej--------------
+#----Metoda akceptacji i odrzuceń---------------
+#----Metoda transformacji-----------------------
+#----Sumy i mieszaniny rozkładów----------------
+
+
+
+#----Generator liczb pseudolosowych z d-wymiarowego rozkładu Wisharta-----------
+
