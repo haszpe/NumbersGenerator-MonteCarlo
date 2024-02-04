8ffcff1b150e86696bc5c14ad185350e872495e2
HP <hanna.peciak@gmail.com>
2024-02-04 15:32:22 +0100

    Add Wishart(d,n,sigma) generator

--- Generator_liczb_z_rozkladu.R
+++ Generator_liczb_z_rozkladu.R
@@ -15,8 +15,6 @@ set.seed(10)
 
 #----Generatory liczb losowych z rozkładu normalnego----------------------------
 
-
-
 #----Metoda odwrotnej dystrybuanty ----------------
 
 inverse_cumulative_distribution <- function(n, mean, sigma){
@@ -35,7 +33,6 @@ inverse_cumulative_distribution <- function(n, mean, sigma){
   }
 
 print(inverse_cumulative_distribution(5, 3, 0.5))
-
 
 
 #----Metoda akceptacji i odrzuceń---------------
@@ -59,7 +56,6 @@ accept_reject <- function(n, mean, sigma){
   }
 
 print(accept_reject(5, 3, 0.5))
-
 
 
 #----Metoda transformacji CHi^2 -----------------------
@@ -108,6 +104,34 @@ print(box_muller_transformation(5, 3, 0.5))
 
 
 
+#----Generator liczb pseudolosowych z d-wymiarowego rozkładu Wisharta----------
 
-#----Generator liczb pseudolosowych z d-wymiarowego rozkładu Wisharta-----------
+generate_wishart <- function(d, n, sigma){
+  # d = wymiar rozkładu
+  # n = stopnie swobody
+  # sigma = macierz kowariancji
+  
+  if (!(n > d + 1 && d > 1)) {
+    stop("Warunek n > d + 1 > 1 nie jest spełniony.")
+  }
+  # Uzupełniamy macierz Z losowymi liczbami ze standaryzowanego rozkładu normalnego 
+  Z <- matrix(rnorm(d * n), nrow = n, ncol = d)
+    
+  # Faktoryzacja macierzy Z (M = X^T * X)
+  M <- t(Z) %*% Z
+    
+  # Przeskalowanie przez macierz kowariancji Σ
+  wishart_matrix <- sigma %*% M
+  
+  return(wishart_matrix)
+  }
 
+
+d <- 3
+Sigma <- matrix(c(1, 0.5, 0.5, 
+                  0.5, 1, 0.5, 
+                  0.5, 0.5, 1), 
+                nrow = d, ncol = d)
+n <- 5
+
+generate_wishart(3, 5, Sigma)
