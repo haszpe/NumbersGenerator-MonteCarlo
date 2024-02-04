e930a85df5746b464f2a82c0f44cbed762360092
HP <hanna.peciak@gmail.com>
2024-02-04 18:19:40 +0100

    Small fixes

--- Generator_liczb_z_rozkladu.R
+++ Generator_liczb_z_rozkladu.R
@@ -20,13 +20,11 @@ set.seed(10)
 inverse_cumulative_distribution <- function(n, mean, sigma){
   random_N <- numeric(n)
   
-  for (i in i:n){
-    
-    u <- runif(n)
-    
-    # qnorm przekształca u na wartości z rozkładu normalnego o zadanych parametrach
+  for (i in 1:n){
+    u <- runif(1)
+    # qnorm przekształca u na wartości z rozkładu normalnego 
+    # o zadanych parametrach
     z <- qnorm(u, mean = mean, sd = sigma)
-    
     random_N[i] <- z
     }
   return(random_N)
@@ -47,7 +45,8 @@ accept_reject <- function(n, mean, sigma){
       
       if (exp(-z^2 / 2) / sqrt(2 * pi) > u) {
         
-        # Skalowanie o odchylenie i przesunięcie o średnią z zadanego rozkładu normalnego 
+        # Skalowanie o odchylenie i przesunięcie o średnią z zadanego 
+        # rozkładu normalnego 
         random_N[i] <- z * sigma + mean
         break
       }}
@@ -63,19 +62,21 @@ print(accept_reject(5, 3, 0.5))
 chi_square_transformation <- function(n, mean, sigma){
   random_N <- numeric(n)
   
-  for (i in i:n){
+  for (i in 1:n){
     
-    # Losowanie liczby z rozkładu Chi^2 z jendym stopniem swobody
+    # Losowanie liczby z rozkładu Chi^2 z jednym stopniem swobody
     chi_square_random <- rchisq(1, df = 1)
     
     # Obliczenie pierwiastku i definicja znaku (dla zachowania symetrii rozkładu)
-    chi_v <- sqrt(chi_squared_values) * sign(runif(1) - 0.5)
+    chi_v <- sqrt(chi_square_random) * sign(runif(1) - 0.5)
     
     # Skalowanie o odchylenie i przesunięcie o średnią z zadanego rozkładu normalnego 
     random_N[i] <- mean + chi_v * sigma
     }
   return(random_N)
   }
+
+print(chi_square_transformation(5, 3, 0.5))
 
 
 #----Metoda transformacja Boxa-Mullera--------------
